ada6832bb9568e7ff29330fd9b03818df6d3479f
thanhlamauto <thanhlamtba@gmail.com>
2023-12-12 22:36:14 +0700

    First update for profile page

--- lib/profile.dart
+++ lib/profile.dart
@@ -8,8 +8,87 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+
   @override
   Widget build(BuildContext context) {
-    return const Text('Profile');
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Color(0xFF26CBE6),
+              Color(0xFF26CBC0),
+            ], begin: Alignment.topCenter, end: Alignment.center)),
+          ),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Container(
+              child: Stack(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: height / 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundImage:
+                                const AssetImage('touch-grass/img/ava.jpg'),
+                            radius: height / 10,
+                          ),
+                          SizedBox(
+                            height: height / 30,
+                          ),
+                          const Text(
+                            'lamthanhz',
+                            style: TextStyle(
+                                fontSize: 18.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: height / 2.2),
+                    child: Container(
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: height / 2.6,
+                        left: width / 20,
+                        right: width / 20),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.black45,
+                                    blurRadius: 2.0,
+                                    offset: Offset(0.0, 2.0))
+                              ]),
+                        ),
+                        
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+  
