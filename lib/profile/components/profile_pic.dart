d6238ff445e667fbf3f9a32c5beb5717541815cc
Thanh Lâm Nguyễn <thanhlamtba@gmail.com>
2024-02-14 11:00:11 +0700

    desgin the ava placeholder and add func to fetch the user's photo

---
+++ lib/profile/components/profile_pic.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gdsc_solution/functions/auth.dart';
+import 'package:flutter_svg/svg.dart';
+
+class ProfilePic extends StatefulWidget {
+  const ProfilePic({super.key});
+
+  @override
+  State<ProfilePic> createState() => _ProfilePicState();
+}
+
+class _ProfilePicState extends State<ProfilePic> {
+  User? currentUser = FirebaseAuth.instance.currentUser;
+  Map<String, dynamic>? userData;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    if (currentUser != null) {
+      var userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUser!.uid)
+          .get();
+      if (userDoc.exists) {
+        setState(() {
+          userData = userDoc.data();
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 115,
+      width: 115,
+      child: Stack(
+        fit: StackFit.expand,
+        clipBehavior: Clip.none,
+        children: [
+          CircleAvatar(
+            backgroundImage: userData != null && userData!['photo'] != null
+                ? NetworkImage(userData!['photo'] as String) // Cast to String
+                : const AssetImage('assets/app/icon.png') as ImageProvider, // Provide type for AssetImage
+            backgroundColor: Colors.transparent,
+          ),
+          Positioned(
+            right: -16,
+            bottom: 0,
+            child: SizedBox(
+              height: 46,
+              width: 46,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    side: const BorderSide(color: Colors.white),
+                  ),
+                  backgroundColor: const Color(0xFFF5F6F9),
+                ),
+                onPressed: () {},
+                child: SvgPicture.asset("assets/icon/CameraIcon.svg"),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
